52458c220b628da30db547a31495f68f3863a52f
Tom Moulard <tom.moulard@traefik.io>
2021-04-23 10:46:24 +0200

    test: wip

--- roundrobin/stickysessions_test.go
+++ roundrobin/stickysessions_test.go
@@ -5,12 +5,15 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
+	"reflect"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/vulcand/oxy/forward"
+	"github.com/vulcand/oxy/roundrobin/stickycookie"
 	"github.com/vulcand/oxy/testutils"
 )
 
@@ -410,4 +413,101 @@ func TestBadCookieVal(t *testing.T) {
 	_, err = ioutil.ReadAll(resp.Body)
 	require.NoError(t, err)
 	assert.Equal(t, http.StatusInternalServerError, resp.StatusCode)
+}
+
+func TestStickySession_GetBackend(t *testing.T) {
+	cookieName := "Test-Cookie"
+	servers := []*url.URL{
+		{Scheme: "http", Host: "10.10.10.10", Path: "/"},
+		{Scheme: "https", Host: "10.10.10.42", Path: "/"},
+		{Scheme: "http", Host: "10.10.10.10", Path: "/foo"},
+		{Scheme: "http", Host: "10.10.10.11", Path: "/", User: url.User("John Doe")},
+	}
+	defaultManager := &stickycookie.DefaultManager{}
+	hashManager := &stickycookie.HashManager{}
+	tests := []struct {
+		name    string
+		Manager stickycookie.Manager
+		cookie  *http.Cookie
+		want    *url.URL
+	}{
+		{
+			name: "NoCookies",
+		},
+		{
+			name:   "Cookie no matched",
+			cookie: &http.Cookie{Name: "not" + cookieName, Value: "http://10.10.10.10/"},
+		},
+		{
+			name:   "Cookie not a URL",
+			cookie: &http.Cookie{Name: cookieName, Value: "foo://foo bar"},
+		},
+		{
+			name:   "Simple",
+			cookie: &http.Cookie{Name: cookieName, Value: "http://10.10.10.10/"},
+			want:   servers[0],
+		},
+		{
+			name:   "Host no match for needle",
+			cookie: &http.Cookie{Name: cookieName, Value: "http://10.10.10.255/"},
+		},
+		{
+			name:   "Scheme no match for needle",
+			cookie: &http.Cookie{Name: cookieName, Value: "https://10.10.10.10/"},
+		},
+		{
+			name:   "Path no match for needle",
+			cookie: &http.Cookie{Name: cookieName, Value: "http://10.10.10.10/foo/bar"},
+		},
+		{
+			name:   "With user in haystack but not in needle",
+			cookie: &http.Cookie{Name: cookieName, Value: "http://10.10.10.11/"},
+			want:   servers[3],
+		},
+		{
+			name:   "With user in haystack and in needle",
+			cookie: &http.Cookie{Name: cookieName, Value: "http://John%20Doe@10.10.10.11/"},
+			want:   servers[3],
+		},
+		{
+			name:    "Cookie no matched with DefaultManager",
+			Manager: defaultManager,
+			cookie:  &http.Cookie{Name: "not" + cookieName, Value: defaultManager.ToValue("http://10.10.10.10/")},
+		},
+		{
+			name:    "Cookie no matched with HashManager",
+			Manager: hashManager,
+			cookie:  &http.Cookie{Name: "not" + cookieName, Value: hashManager.ToValue("http://10.10.10.10/")},
+		},
+		{
+			name:    "Cookie value not matched with HashManager",
+			Manager: hashManager,
+			cookie:  &http.Cookie{Name: cookieName, Value: hashManager.ToValue("http://10.10.10.255/")},
+		},
+		{
+			name:    "simple with HashManager",
+			Manager: hashManager,
+			cookie:  &http.Cookie{Name: cookieName, Value: hashManager.ToValue("http://10.10.10.10/")},
+			want:    servers[0],
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &StickySession{
+				cookieName: cookieName,
+				options:    CookieOptions{},
+				Manager:    tt.Manager,
+			}
+
+			req := httptest.NewRequest(http.MethodGet, "http://foo", nil)
+			if tt.cookie != nil {
+				req.AddCookie(tt.cookie)
+			}
+			got, _, err := s.GetBackend(req, servers)
+			require.NoError(t, err)
+			if !reflect.DeepEqual(tt.want, got) {
+				t.Errorf("GetBackend() failed, got %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
